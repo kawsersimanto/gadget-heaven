f327cd6e9002ab193c1a1ed9dd59109aaf5deca7
Kawser Simanto <kawsersimanto@gmail.com>
2024-11-22 12:25:44 +0600

    created product details page

--- src/app/product/ProductDetails.jsx
+++ src/app/product/ProductDetails.jsx
@@ -1,5 +1,61 @@
+import { useLoaderData, useParams } from "react-router-dom";
+
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { id } = useParams();
+  const data = useLoaderData();
+  const product = data.find((item) => item.product_id === id);
+  console.log(product);
+
+  return (
+    <section>
+      <div className="pt-8 pb-[238px] bg-primary">
+        <div className="container">
+          <h2 className="text-white text-center mb-4 text-[32px] font-bold">
+            Product Details
+          </h2>
+          <p className="text-center text-white max-w-[796px] mx-auto">
+            Explore the latest gadgets that will take your experience to the
+            next level. From smart devices to the coolest accessories, we have
+            it all!
+          </p>
+        </div>
+      </div>
+      <div className="lg:pb-[100px] md:pb-[80px] pb-[60px]">
+        <div className="container">
+          <div className="p-8 rounded-2xl">
+            <div className="flex gap-8">
+              <img
+                src={product?.product_image || "/woocommerce-placeholder.png"}
+                className="w-[424px] rounded-2xl object-contain"
+                alt={product?.product_title}
+              />
+              <div className="flex flex-col gap-4">
+                <div>
+                  <h1 className="font-semibold text-[#09080F] text-[28px] mb-3">
+                    {product?.product_title || "N/A"}
+                  </h1>
+                  <h3>Price: ${product?.price || "N/A"}</h3>
+                </div>
+                <div>
+                  <span className="py-[7px] px-[12px] rounded-[32px] text-[#309C08] border border-[#309C08] bg-[rgba(48,156,8,0.10)] text-sm">
+                    In Stock
+                  </span>
+                  <span className="py-[7px] px-[12px] rounded-[32px] text-red-500 border border-red-500 bg-red-200 text-sm">
+                    Out of Stock
+                  </span>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductDetails;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -4,6 +4,7 @@ import Home from "../app/home/Home.jsx";
 import Statistics from "../app/statistics/Statistics.jsx";
 import Dashboard from "../app/dashboard/Dashboard.jsx";
 import NotFound from "../app/not-found.jsx";
+import ProductDetails from "../app/product/ProductDetails.jsx";
 
 const router = createBrowserRouter([
   {
@@ -25,7 +26,17 @@ const router = createBrowserRouter([
       },
       {
         path: "/product/:id",
-        element: <Dashboard />,
+        loader: async () => {
+          const response = await fetch(
+            "https://raw.githubusercontent.com/kawsersimanto/gadget-heaven/refs/heads/main/public/products.json"
+          );
+          if (!response.ok) {
+            throw new Error("Failed to fetch products");
+          }
+          const products = await response.json();
+          return products; // Return the parsed array of products
+        },
+        element: <ProductDetails />,
       },
     ],
   },
