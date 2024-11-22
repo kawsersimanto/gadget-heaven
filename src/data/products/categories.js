b57cceee7b9c8e9bc4a19c0f9993bd1508a8b533
Kawser Simanto <kawsersimanto@gmail.com>
2024-11-22 08:08:52 +0600

    created categories

---
+++ src/data/products/categories.js
@@ -0,0 +1,28 @@
+const categories = [
+  {
+    id: 1,
+    name: "Laptops",
+  },
+  {
+    id: 2,
+    name: "Phones",
+  },
+  {
+    id: 3,
+    name: "Smart Watches",
+  },
+  {
+    id: 4,
+    name: "Laptops",
+  },
+  {
+    id: 5,
+    name: "Laptops",
+  },
+  {
+    id: 6,
+    name: "Laptops",
+  },
+];
+
+export default categories;
