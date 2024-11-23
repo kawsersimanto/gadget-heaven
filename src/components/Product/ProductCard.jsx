b4a9ced6ae4a682d24eade778fe2733c5fed4d46
Kawser Simanto <kawsersimanto@gmail.com>
2024-11-23 22:34:00 +0600

    added wishlist remove and cart remove functionlaity

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -2,23 +2,51 @@ import PropTypes from "prop-types";
 import { cn } from "../../utils/cn";
 import { useContext } from "react";
 import { StoreContext } from "../../contexts/StoreContext";
+import { toast } from "sonner";
 
 const ProductCard = ({ product, isWishlist }) => {
   const { cart, setCart, wishlist, setWishlist } = useContext(StoreContext);
 
   const handleAddToCart = (product) => {
-    const updateWishlist = wishlist.filter(
-      (p) => p?.product_id !== product.product_id
+    const isAlreadyInCart = cart.some(
+      (p) => p.product_id === product.product_id
     );
-    setCart([...cart, product]);
-    setWishlist([wishlist, updateWishlist]);
+    const updatedWishlist = wishlist.filter(
+      (p) => p.product_id !== product.product_id
+    );
+
+    setWishlist(updatedWishlist);
+
+    if (isAlreadyInCart) {
+      toast.success(`${product.product_title} is already in the cart`);
+    } else {
+      setCart((prevCart) => [...prevCart, product]);
+      toast.success(`${product.product_title} has been added to the cart`);
+    }
+  };
+
+  const handleRemoveProduct = (product) => {
+    const updateWishlist = wishlist.filter(
+      (p) => p.product_id !== product.product_id
+    );
+    const updateCart = cart.filter((p) => p.product_id !== product.product_id);
+
+    if (isWishlist) {
+      setWishlist(updateWishlist);
+      toast.success(
+        `${product.product_title} has been removed from the wishlist`
+      );
+    } else {
+      setCart(updateCart);
+      toast.success(`${product.product_title} has been removed from the cart`);
+    }
   };
 
   return (
     <div className="gl:p-8 p-4 rounded-2xl bg-white flex sm:flex-row flex-col items-center w-full gap-8">
       <img
         src={product?.product_image || "/woocommerce-placeholder.png"}
-        alt={product?.product_title}
+        alt={product?.product_title || "N/A"}
         className="sm:w-[275px] w-full sm:h-[192px] h-[240px] object-contain object-center drop-shadow-md rounded-xl"
       />
       <div className="flex flex-col gap-4 items-start relative flex-grow">
@@ -26,16 +54,19 @@ const ProductCard = ({ product, isWishlist }) => {
           src="/cross.svg"
           alt="Remove Icon"
           className="absolute top-0 right-0 z-10 lg:w-[36px] w-[30px] cursor-pointer"
+          onClick={() => handleRemoveProduct(product)}
         />
         <h4 className="text-[24px] font-semibold text-[#09080F] max-w-[160px]">
-          {product?.product_title}
+          {product?.product_title || "N/A"}
         </h4>
         <p>
           <span className="text-base font-semibold">Description:</span>{" "}
-          <span className="text-light-gray-200">{product?.description}</span>
+          <span className="text-light-gray-200">
+            {product?.description || "N/A"}
+          </span>
         </p>
         <p className="text-base font-semibold">
-          Price: ${product?.price.toFixed(2)}
+          Price: ${product?.price?.toFixed(2) || 0}
         </p>
         <button
           disabled={!product?.in_stock}
@@ -60,9 +91,9 @@ ProductCard.propTypes = {
     description: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     product_image: PropTypes.string,
-    in_stock: PropTypes.bool.isRequired,
+    in_stock: PropTypes.number.isRequired,
   }).isRequired,
-  isWishlist: PropTypes.bool.isRequired,
+  isWishlist: PropTypes.bool,
 };
 
 export default ProductCard;
