67dea520e6c5ad497ba97d0630cc1c6b0b073761
Kawser Simanto <kawsersimanto@gmail.com>
2024-11-23 22:35:40 +0600

    removed console log

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,9 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../shared/Navbar";
 import Footer from "../shared/Footer";
-import { useContext } from "react";
-import { StoreContext } from "../contexts/StoreContext";
 
 const App = () => {
   const location = useLocation();
-  const { cart, wishlist } = useContext(StoreContext);
-  console.log("cart:", cart);
-  console.log(wishlist);
 
   return (
     <>
