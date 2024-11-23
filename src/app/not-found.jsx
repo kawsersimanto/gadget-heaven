4ff8085745766a1af2b8e37a6ca829227c494225
Kawser Simanto <kawsersimanto@gmail.com>
2024-11-24 00:54:23 +0600

    added 404 page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>not-found</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center px-5">
+      <div>
+        <img src="/404.png" alt="Page Not Found 404." />
+        <h2 className="text-3xl font-bold text-center mb-4 mt-8">
+          Oops! Page not found.
+        </h2>
+        <Link to="/" className="text-center block underline">
+          Go Back To Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
